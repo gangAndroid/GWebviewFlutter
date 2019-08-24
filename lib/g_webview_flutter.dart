2a45b62053c82389c16bb60ae0e0bcbe711f146a
1729950804 <1729950804@qq.com>
2019-08-24 18:02:26 +0800

    修改键盘的弹出bug

--- lib/g_webview_flutter.dart
+++ lib/g_webview_flutter.dart
@@ -3,7 +3,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class GWebviewFlutter {
-  static const MethodChannel _channel = const MethodChannel('g_webview_flutter');
+  static const MethodChannel _channel =
+      const MethodChannel('g_webview_flutter');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
@@ -23,7 +24,8 @@ class GWebviewFlutter {
   ///打开原生的H5页面
   ///
   /// jsLoaded 是网页加载完毕后执行的js代码
-  static Future<void> openWebActivity(String url, {String title, String jsLoaded}) async {
+  static Future<void> openWebActivity(String url,
+      {String title, String jsLoaded}) async {
     if (defaultTargetPlatform == TargetPlatform.android) {
       final Map<String, dynamic> params = <String, dynamic>{
         'url': url,
